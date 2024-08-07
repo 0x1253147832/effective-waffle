5ef69d16487ba58c50766b6588559124081127e2
0x1253147832 <itooktime69@gmail.com>
2024-08-07 13:24:31 +0100

    hi

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-#
+from quart import Quart, request, jsonify, render_template
+
+app = Quart(
+    __name__,
+    static_folder='src/static',
+    template_folder='src/templates',
+    )
+
+@app.route('/')
+async def home():
+    
+    try:
+        
+        return await render_template('index.html')
+    except Exception as error:
+        return jsonify({'error': str(error)})
